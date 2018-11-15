7a429d042531e5b8248b2625a6cb426df97dc326
Ellen Hatleberg <ellen.hatleberg@gmail.com>
2018-11-15 04:00:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(, ) {
+  var katzDeli;
+  var otherDeli;
+  return `Welcome, ${}. You are number ${} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if () {
+
+    return `${1stPersonName}`;
+  } else {
+    return `There is nobody waiting to be served!`;
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if () {
+
+    return `The line is currently: ${1stNum}. ${1stName}, ${2ndNum...}. ${2ndName...}`;
+  } else {
+    return `The line is currently empty.`;
+  }
+}
